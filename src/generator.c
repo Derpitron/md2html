822da37e885b39072481b76260daaf23e63a5acc
Aadhithya Narayanan Madhusudhanan <aadhithyanm@protonmail.com>
2025-03-27 22:43:54 +0530

    [unusable] some work for the day

--- src/generator.c
+++ src/generator.c
@@ -1,32 +1,92 @@
 // This file is my own idea but I don't think it's good design
 #include "stdio.h"
 
-void parseLine(char text[], int len) {
-  //Parsing h1
-  
-  char elementContentBuf[64];
-  
-  int index = 0;
-  int hCount = 0;
+typedef enum {
+	ILLEGAL,
+	WHITESPACE,
+	NEWLINE,
+	EOF,
 
-  while (text[index] == '#') {
-    index++;
-    hCount++;
-  }
+	//md exlcusive
+	//look here if you wanna add anything new
+	HASH,
+	STAR,
+	TXT
+} token;
 
-  //capture the rest of the non-format text
-  for (int j = index; j < len; j++) {
-    if (text[j] != '\n') {
-     elementContentBuf[j-index] = text[j];
-    }
-  }
+typedef struct {
+	//A token can have two forms: e.g for encountered token "**"
+	// t would just be STAR
+	// s would just be "**" the string literal itself.
+	token t;
+	char s[];
+} scanartifact;
 
-  printf("<h%2$d>%s</h%2$d>\n", elementContentBuf, hCount);
-  return;
+//utility struct intended to help make lexing easier between functions
+typedef struct {
+	int index;
+	char text[];
+} scanintermediate;
+
+//This function's ultimate job is to return a list of tokens for a single line, as well as it's accompanying text
+//input "# hello *world*\n"
+//output {{HASH, "#"}, {TXT, " hello "}, {STAR, "*"}, {TXT, "world"}, {STAR, "*"}, {NEWLINE, "\n"}}
+void lexLine(char srctext[], int len, scanartifact artifacts[]) {
+	int index = -1;
+	int tokenCount = 0;
+	
+	while { //word level lex-loop
+		scanartifact artifact;
+		index++;
+
+		if (index >= len) {
+			return {EOF, ""};
+		}
+		
+		char currentCharacter = srctext[index];
+		//I don't think we need a isLetter or isSpace checker --1
+		if (currentCharacter == '\0') {//EOF
+			return {EOF, ""};
+		}
+
+		//Actually parse the thing as a real token now.
+		index--;
+
+		//do we really need this?
+		char buf[256] = ""; //sus
+		int bufIndex = 0;
+		//md isnt whitespace delimited. this is a problem for separating tokens --1
+		while { //intra-word character level lex-loop
+			index++;
+
+			if (index >= len) {
+				return {EOF, buf};
+			}
+			
+			currentCharacter = srctext[index];
+			//I don't think we need a isLetter or isSpace checker
+			if (currentCharacter == '\0') {//EOF
+				return {EOF, ""};
+			}
+			buf[bufIndex] = currentCharacter;
+			bufIndex++;
+		}
+		switch (currentCharacter) {
+			case '#': {
+				
+			}
+		}
+
+	}
+
+
+	return;
 }
 
+
 int main() {
-  char example[] = "##he# hello";
-  parseLine(example, sizeof(example)/sizeof(char));
-  return 0;
+	char example[] = "# hello *world*";
+	scanartifact artifacts[64]; //sus
+	parseLine(example, sizeof(example)/sizeof(char));
+	return 0;
 }
--- src/lexer.c
+++ src/lexer.c
@@ -1,48 +1,44 @@
 //Code derived (translated) from https://github.com/fisik-yum/ucge/blob/main/cengine/llex.go
 //wip. unusable
 typedef enum {
-    ILLEGAL,
-    WHITESPACE,
-    NEWLINE,
-    EOF,
+	ILLEGAL,
+	WHITESPACE,
+	NEWLINE,
+	EOF,
 
-    //md exlcusive
-    HASH,
-    STAR,
-    IDENT
+	//md exlcusive
+	HASH,
+	STAR,
+	TXT
 } token_t;
 
 //why do we need this?
 typedef struct {
-    char[] data;
-    int index;
+	char[] data;
+	int index;
 } scanner_t;
 
 typedef struct {
-    token_t tokenForm;
-    char stringForm[];
+	//A token can have two forms:
+	// tokenForm would just be STAR
+	// stringForm would just be "**" the string literal itself.
+	token_t tokenForm;
+	char stringForm[];
 } scanartifact_t;
-
-int isHash(char c) {
-    return c == '#';
-}
-
-int isStar(char c) {
-    return c == '*';
-}
 
 //This section parses a string as input.
 
 scanartifact_t Scan(scanner_t* s) {
-    s.index++;
-    if (s.index >= sizeof(s.data)) {
-        return {EOF, ""};
-    }
-    curChar = s.data[s.index];
-    switch (c) {
-         case '#': {break;}
-         case '*': {break;}
-        case '\0': {return {EOF, ""} break;}
-        default: { return {ILLEGAL, ""}}
-    }
+	s.index++;
+	if (s.index >= sizeof(s.data)) {
+		return {EOF, ""};
+	}
+	curChar = s.data[s.index];
+
+	switch (curChar) {
+		case '#': {return break;}
+		case '*': {return break;}
+		case '\0': {return {EOF, ""} break;}
+		default: { return {TXT, }}
+	}
 }
